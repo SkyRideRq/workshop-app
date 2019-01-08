9812fd76c71ed1323b093b249a684d6a302fc2e0
SkyRideRq <piotrslowinski@op.pl>
2019-01-08 12:35:46 +0100

    Updates

---
+++ precache-manifest.ead5188c68fcd31803844830f2a3e5c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8dd91205e0d72f81ec3b",
+    "url": "/myapp/static/js/runtime~main.8dd91205.js"
+  },
+  {
+    "revision": "dea1931b20a88518948e",
+    "url": "/myapp/static/js/main.dea1931b.chunk.js"
+  },
+  {
+    "revision": "033cdf4a086cad5b4a40",
+    "url": "/myapp/static/js/1.033cdf4a.chunk.js"
+  },
+  {
+    "revision": "dea1931b20a88518948e",
+    "url": "/myapp/static/css/main.1a2cf200.chunk.css"
+  },
+  {
+    "revision": "033cdf4a086cad5b4a40",
+    "url": "/myapp/static/css/1.82927961.chunk.css"
+  },
+  {
+    "revision": "aa0bc5f90d63a57cb94fba95d8d139a5",
+    "url": "/myapp/index.html"
+  }
+];
